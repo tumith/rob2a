98269eb807d87fb9c28b48cd2a1cbeb490e338f0
Eirikur <ebe@tskoli.is>
2019-03-11 17:15:18 +0000

    breytti position code

--- example/struct_array_pointer/position.c
+++ example/struct_array_pointer/position.c
@@ -5,18 +5,18 @@ this is an array of type struct Pos , it keeps pointers to the structs
 Pos pos_array[10];
 /*
 this function is of type struct Pos and it makes and keeps data
-of x,y and positioning
+of x,y and z positioning
 */
 Pos *set_pos(){
 	for(int i =0;i<10;i++){
-	int _x = rand()%10;
-	int _y = rand()%10;
-	int _z = rand()%10;
+		int _x = rand()%10;
+		int _y = rand()%10;
+		int _z = rand()%10;
 		pos_array[i].x=_x;
 		pos_array[i].y=_y;
 		pos_array[i].z=_z;
 	}
-		return &pos_array;//retuns a pointer to the array
+	return &pos_array;//retuns a pointer to the array
 }
 
 task main()
@@ -26,6 +26,7 @@ task main()
 		writeDebugStream("value of x is =%d \n",pointer_to_array->x);
 		writeDebugStream("value of y is =%d \n",pointer_to_array->y);
 		writeDebugStream("value of z is =%d \n",pointer_to_array->z);
+		writeDebugStream("rand of 2 =%d \n",rand()%2);
 		pointer_to_array++;	//make the pointer point to the next dataset value
 		}
 }
--- example/struct_array_pointer/structpointer.h
+++ example/struct_array_pointer/structpointer.h
@@ -1,3 +1,6 @@
+/*
+All constants should be difined in the headerfile
+*/
 #define COL 6
 #define ROW 5
 
@@ -6,7 +9,12 @@ typedef struct Cords{
 	int col;
 	bool tf;
 }Cords;
-
+/*
+This is an struct for positioning X;Y,Z
+good for using an array with structs in stead of 2d array you can use
+single dimention array.
+defining a struct is done in the header file.
+*/
 typedef struct Pos{
 	int x;
 	int y;
