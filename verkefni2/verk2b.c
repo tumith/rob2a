3e77f4a678ae6fe46341be98a3402fc4cd85ff3f
tumith <tumithorberg@gmail.com>
2019-09-17 10:26:13 +0000

    fælðirit set inn og byrjað a verk2b

--- verkefni2/verk2b.c
+++ verkefni2/verk2b.c
@@ -1,3 +1,22 @@
+#pragma config(Sensor, in1,    LightSensor,    sensorReflection)
+#pragma config(Sensor, in2,    LineFollow1,    sensorLineFollower)
+#pragma config(Sensor, in3,    LineFollow2,    sensorLineFollower)
+#pragma config(Sensor, in4,    LineFollow3,    sensorLineFollower)
+#pragma config(Sensor, in5,    Potatometer,    sensorPotentiometer)
+#pragma config(Sensor, in6,    GyroSensor,     sensorGyro)
+#pragma config(Sensor, dgtl1,  FruntButton,    sensorTouch)
+#pragma config(Sensor, dgtl2,  CraneStoper,    sensorQuadEncoder)
+#pragma config(Sensor, dgtl6,  LEncoder,       sensorQuadEncoder)
+#pragma config(Sensor, dgtl8,  REncoder,       sensorQuadEncoder)
+#pragma config(Sensor, dgtl10, SonarCM,        sensorSONAR_cm)
+#pragma config(Motor,  port1,           LMotor,        tmotorVex393, openLoop, reversed)
+#pragma config(Motor,  port6,           CraneArm,      tmotorVex393, openLoop)
+#pragma config(Motor,  port7,           Claw,          tmotorVex269, openLoop)
+#pragma config(Motor,  port10,          RMotor,        tmotorVex393, openLoop)
+
+#include "../include/header/movingForwardHeder.h"
+#include "../include/functions/myfunctions.c";
+bool b_f = true;
 
 task main()
 {
