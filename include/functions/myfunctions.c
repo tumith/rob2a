cbce8bb9a9c31705a0658fce98dfdb1ec451429b
tumith <tumithorberg@gmail.com>
2019-09-26 12:33:10 +0000

    byrjað i verk2c

---
+++ include/functions/myfunctions.c
@@ -0,0 +1,106 @@
+
+void driveS(int dist, bool b_f){
+	int dir = (b_f)? (1):(-1);
+	while(dist>abs(SensorValue[REncoder])){
+		if(abs(SensorValue[REncoder]) == abs(SensorValue[LEncoder])) // If rightEncoder has counted the same amount as leftEncoder:
+			{
+				// Move Forward
+				motor[RMotor] = 100*dir;		    // Right Motor is run at power level 80
+				motor[LMotor] = 80*dir;		    // Left Motor is run at power level 80
+			}
+		else	// Only runs if leftEncoder has counted more encoder counts
+			{
+				motor[RMotor] = 100*dir;
+				motor[LMotor] = 80*dir;
+			}
+	}
+}
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+void driveC(int dist){
+	while(dist>abs(SensorValue[REncoder])){
+		if(abs(SensorValue[REncoder]) == abs(SensorValue[LEncoder]))
+			{
+				motor[RMotor] = 80;
+				motor[LMotor] = 80;
+			}
+			else{
+				motor[RMotor] = 80;
+				motor[LMotor] = 80;
+			}
+	}
+}
+
+
+//--------------------------------------------| FORWARD |---------------------------------------------
+void drivec(int dist)
+{
+  SensorValue[REncoder] = 0;    /* Clear the encoders for    */
+  SensorValue[LEncoder]  = 0;    /* consistancy and accuracy. */
+
+  while(SensorValue[REncoder] < dist * BASE_DIST && SensorValue[LEncoder] < dist *BASE_DIST)
+  {
+    motor[RMotor] = 63;         /* Run both motors        */
+    motor[LMotor]  = 63;         /* forward at half speed. */
+  }
+  motor[RMotor] = 0;            /* Stop both motors!  This is important so that each function          */
+  motor[LMotor]  = 0;            /* can act independantly as a "chunk" of code, without any loose ends. */
+}
+//----------------------------------------------------------------------------------------------------
+
+//-------------------------------------------| TURN LEFT |--------------------------------------------
+void TurnLeft(float r)
+{
+  SensorValue[REncoder] = 0;    /* Clear the encoders for    */
+  SensorValue[LEncoder]  = 0;    /* consistancy and accuracy. */
+
+  // While the encoders have not yet met their goal: (left is compared negativly since it will in reverse)
+  while(SensorValue[REncoder] < (r * BASE_ROTATION) && SensorValue[LEncoder] > (-1 * r * BASE_ROTATION))
+  {
+    motor[RMotor] = 63;         // Run the right motor forward at half speed
+    motor[LMotor]  = -63;        // Run the left motor backward at half speed
+  }
+  motor[RMotor] = 0;            /* Stop both motors!  This is important so that each function          */
+  motor[LMotor]  = 0;            /* can act independantly as a "chunk" of code, without any loose ends. */
+}
+//----------------------------------------------------------------------------------------------------
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+void resetEncoder(){
+	SensorValue[REncoder] = 0;
+	SensorValue[LEncoder] = 0;
+}
+
+void stopMotors(){
+	motor[LMotor] = 0;
+	motor[RMotor] = 0;
+	wait1Msec(1000);
+}
+
+void drive(int drivetime, bool b_f){
+	int dir = (b_f)? (1):(-1);
+	motor[RMotor] = FULL_POWER*dir;
+	motor[LMotor] = FULL_POWER*dir;
+	wait1Msec(drivetime);
+	motor[LMotor] = 0;
+  motor[RMotor] = 0;
+  wait1Msec(1000);
+}
+
+task display(){
+			bLCDBacklight = true;
+		string mainBattery;
+
+		while(true)
+		{
+			 clearLCDLine(0);
+			 clearLCDLine(1);
+
+			 displayLCDString(0, 0, "Primary: ");
+			 sprintf(mainBattery, "%1.2%c", nImmediateBatteryLevel/1000.0,'V');
+			 displayNextLCDString(mainBattery);
+
+			 wait1Msec(100);
+		}
+}
