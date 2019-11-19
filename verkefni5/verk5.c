a30b490689f9bc1b165fc6a903b81e3a627e4c27
tumth <tumithorberg@gmail.com>
2019-11-19 08:28:13 +0000

    verk 5

--- verkefni5/verk5.c
+++ verkefni5/verk5.c
@@ -16,27 +16,25 @@
 
 
 /*----------------------------------------------------------------------------------------------------*\
-|*                                    -  -                                     *|
+|*                                    - LineSensor / LineFollower -                          					*|
 |*                                      ROBOTC on VEX 2.0 Cortex                                      *|
-|*                                   *|
-|*                                   *|
-|*                                                                                                    *|
+|*    Thetta er programm til ad elta linu en ef thad er ekki sed linuna leingur tha er stopad.        *|
 |*                                                                                                    *|
 |*                                                                                                    *|
 |*                                        ROBOT CONFIGURATION                                         *|
 |*    NOTES:                                                                                          *|
-|*   1      *|
-|*          *|
-|*   2)     *|
-|*          *|
+|*   1) Robot leitar af linu ef hann ser linu tha eltir hann linuna en ef hann ser einga linu tha er  *|
+|*      hann kjur og bidur eftir thvi ad sja linu.																								    *|
+|*   2) Thegar robotin ser linu eltir hann linuna thangad til hann ser hana ekki leignur.					    *|
 |*                                                                                                    *|
 |*                                                                                                    *|
 |*    MOTORS & SENSORS:                                                                               *|
 |*    [I/O Port]             [Name]              [Type]              [Description]                    *|
 |*    Motor - Port 10        RMotor            tmotorVex393           Right motor                     *|
 |*    Motor - Port 1         LMotor            tmotorVex393           Left motor                      *|
-|*       *|
-|*                  *|
+|*    Sensor - in2           LineFollow1  		 sensorLineFollower     Line detector										*|
+|*    Sensor - in3           LineFollow2  		 sensorLineFollower     Line detector										*|
+|*    Sensor - in4           LineFollow3  		 sensorLineFollower     Line detector										*|
 \*----------------------------------------------------------------------------------------------------*/
 
 #include "..\include\header\movingForwardHeder.h"
@@ -45,10 +43,13 @@
 task main()
 {
 	StartTask(emergency_stop);
-
+	while(true){
+	if (vexRT[Btn6U] == true){
 	drive_line(63);
-
+	}
 }
+}
+
 task emergency_stop(){
 	while(true){
 		if (SensorValue(FruntButton) == 1 || vexRT[Btn7D] == 1){
